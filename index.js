48fab497387290d4a7f5c901366ae5d067231180
maxikgreat <maximvasylenko228322@gmail.com>
2020-06-15 20:58:50 +0200

    IOS splash screen & app icon added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 import 'react-native-gesture-handler';
+import React, {useEffect} from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import App from './src/App';
+import SplashScreen from 'react-native-splash-screen';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function index() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <App />
+  );
+}
+
+AppRegistry.registerComponent(appName, () => index);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
 import {
@@ -24,7 +17,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
